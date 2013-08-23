bf7e47039e1d0e15d4399fa637fa219c02d59658
Ben Hearsum <bhearsum@mozilla.com>
2013-08-23 13:38:29 -0400

    fix bad syntax.

--- slaveapi/slave.py
+++ slaveapi/slave.py
@@ -115,7 +115,7 @@ class Slave(object):
         # First, wait for the slave to go down.
         time_left = down_timeout
         while time_left <= down_timeout:
-            if not self.ping(self.ip, count=1, deadline=2):
+            if not ping(self.ip, count=1, deadline=2):
                 log.debug("Slave is confirmed to be down, waiting for revival.")
                 break
         else:
