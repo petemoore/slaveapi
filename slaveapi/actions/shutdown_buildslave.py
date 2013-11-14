ef93e09ba05202279641ba5b28b1edd97fd7395a
Ben Hearsum <bhearsum@mozilla.com>
2013-11-14 14:48:57 -0500

    Fix graceful shutdown verification when slave shuts down before twistd.log can be read.

--- slaveapi/actions/shutdown_buildslave.py
+++ slaveapi/actions/shutdown_buildslave.py
@@ -3,8 +3,7 @@ import requests
 import time
 
 from .results import SUCCESS, FAILURE
-from ..clients.ssh import RemoteCommandError
-from ..slave import Slave, get_console
+from ..slave import Slave, get_console, is_alive
 
 import logging
 log = logging.getLogger(__name__)
@@ -39,15 +38,26 @@ def shutdown_buildslave(name):
     twistd_log = "%s/%s" % (slave.basedir, "twistd.log")
     start = time.time()
     console = get_console(slave)
+    down = False
     while time.time() - start < MAX_SHUTDOWN_WAIT_TIME:
         try:
             rc, output = console.run_cmd("tail -n1 %s" % twistd_log)
             if "Server Shut Down" in output:
-                return SUCCESS, "Shutdown succeeded."
+                down = True
+                break
             else:
                 time.sleep(30)
-        except RemoteCommandError:
-            log.debug("Caught error when waiting for shutdown, trying again...", exc_info=True)
-            time.sleep(30)
+        except:
+            log.debug("Caught error while waiting for graceful shutdown, checking to see if slave is still alive.", exc_info=True)
+            if is_alive(slave, timeout=10):
+                log.debug("Slave is still alive, trying again...")
+                time.sleep(30)
+            else:
+                log.debug("Slave went down during graceful shutdown, assuming success.")
+                down = True
+                break
+
+    if down:
+        return SUCCESS, "Shutdown succeeded."
     else:
         return FAILURE, "Couldn't confirm shutdown."
