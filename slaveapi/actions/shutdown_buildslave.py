2a04fabf1337eab10ad9b203f0377ffe442dcd2c
Ben Hearsum <bhearsum@mozilla.com>
2013-11-15 10:57:33 -0500

    Don't watch for reboot when waiting for graceful shutdown.

--- slaveapi/actions/shutdown_buildslave.py
+++ slaveapi/actions/shutdown_buildslave.py
@@ -3,7 +3,8 @@ import requests
 import time
 
 from .results import SUCCESS, FAILURE
-from ..slave import Slave, get_console, is_alive
+from ..clients.ssh import RemoteCommandError
+from ..slave import Slave, get_console
 
 import logging
 log = logging.getLogger(__name__)
@@ -38,26 +39,15 @@ def shutdown_buildslave(name):
     twistd_log = "%s/%s" % (slave.basedir, "twistd.log")
     start = time.time()
     console = get_console(slave)
-    down = False
     while time.time() - start < MAX_SHUTDOWN_WAIT_TIME:
         try:
             rc, output = console.run_cmd("tail -n1 %s" % twistd_log)
             if "Server Shut Down" in output:
-                down = True
-                break
+                return SUCCESS, "Shutdown succeeded."
             else:
                 time.sleep(30)
-        except:
-            log.debug("Caught error while waiting for graceful shutdown, checking to see if slave is still alive.", exc_info=True)
-            if is_alive(slave, timeout=10):
-                log.debug("Slave is still alive, trying again...")
-                time.sleep(30)
-            else:
-                log.debug("Slave went down during graceful shutdown, assuming success.")
-                down = True
-                break
-
-    if down:
-        return SUCCESS, "Shutdown succeeded."
+        except RemoteCommandError:
+            log.debug("Caught error when waiting for shutdown, trying again...", exc_info=True)
+            time.sleep(30)
     else:
         return FAILURE, "Couldn't confirm shutdown."
