8a742ecdec3999277c6d5c1a2f63832c0415eb0d
Justin Wood <Callek@gmail.com>
2014-04-07 22:20:53 -0400

    Bug 992235 - Using slaveapi to reboot a machine on the slave health page results in "list index out of range" under 'output'. r=jhopkins

--- slaveapi/clients/inventory.py
+++ slaveapi/clients/inventory.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from furl import furl
 
 import requests
@@ -6,6 +7,9 @@ import logging
 log = logging.getLogger(__name__)
 
 def find_key_value(info, wanted_key):
+    if not info["key_value"]:
+        return None
+
     for key, value in [(i["key"],i["value"]) for i in info["key_value"]]:
         if key == wanted_key:
             return value
@@ -19,7 +23,12 @@ def get_system(fqdn, api, username, password):
     url.args["hostname"] = fqdn
     auth = (username, password)
     log.debug("%s - Making request to %s", fqdn, url)
-    info = requests.get(str(url), auth=auth).json()["objects"][0]
+    info = defaultdict(lambda: None)
+    try:
+        result = requests.get(str(url), auth=auth).json()["objects"][0]
+        info.update(result)
+    except IndexError:
+        pass # It's ok to have no valid host (e.g. ec2)
 
     # We do some post processing because PDUs are buried in the key/value store
     # for some hosts.
@@ -30,14 +39,9 @@ def get_system(fqdn, api, username, password):
             pdu += ".mozilla.com"
         info["pdu_fqdn"] = pdu
         info["pdu_port"] = pdu_port
-    else:
-        info["pdu_fqdn"] = None
-        info["pdu_port"] = None
 
     # If the system has a mozpool server managing it, it's expressed as this key
     imaging_server = find_key_value(info, "system.imaging_server.0")
     if imaging_server:
         info["imaging_server"] = imaging_server
-    else:
-        info["imaging_server"] = None
     return info
