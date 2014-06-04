b5aa9c168371a999bf2cc4ac55e24f31c2d62488
Jordan Lund <jordan.l.lund@gmail.com>
2014-06-04 16:48:26 -0700

    adds util to packages in setup.py - fix bustage - version bump 1.1.1, r=callek

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version=__version__,
     description="A tool for interacting with build and test slaves.",
     author="Mozilla Release Engineering",
-    packages=["slaveapi", "slaveapi.actions", "slaveapi.clients", "slaveapi.web", "slaveapi.machines"],
+    packages=["slaveapi", "slaveapi.actions", "slaveapi.clients", "slaveapi.web", "slaveapi.machines", "slaveapi.util"],
     scripts=["slaveapi-server.py"],
     install_requires=[
         "gevent==0.13.8",
